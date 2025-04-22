58099f71a5b30dc48e476b8309171574c461181b
Sofiya <sofik_shu2@mail.ru>
2025-04-22 14:30:29 +0300

    Added 404 error page

---
+++ src/EventSubscriber/ExceptionSubscriber.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Twig\Environment;
+
+class ExceptionListener
+{
+    private Environment $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    public function __invoke(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+
+        if ($exception instanceof NotFoundHttpException) {
+            $html = $this->twig->render('errors/404.html.twig');
+            $response = new Response($html, 404);
+            $event->setResponse($response);
+        }
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'kernel.exception' => 'onKernelException',
+        ];
+    }
+}
+
