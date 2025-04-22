8d807ec0bd947ca516013cacda1a2462ffc09ace
Sofiya <sofik_shu2@mail.ru>
2025-04-22 22:52:03 +0300

    added all translations

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Booking;
+use App\Entity\Kitten;
+use App\Form\BookingType;
 use App\Repository\BookingRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -22,7 +25,7 @@ class HomeController extends AbstractController
     }
 
     #[Route('/', name: 'app_home')]
-    public function index(LitterService $litterService, Request $request, BookingRepository $bookingRepository): Response
+    public function index(LitterService $litterService, Request $request, BookingRepository $bookingRepository, EntityManagerInterface $entityManager): Response
     {
         $litterData = $litterService->getLitter();
 
@@ -60,12 +63,38 @@ class HomeController extends AbstractController
             return $this->redirectToRoute('app_home');
         }
 
+        // Создаем форму бронирования
+        $booking = new Booking();
+        $formBooking = $this->createForm(BookingType::class, $booking);
+        $formBooking->handleRequest($request);
+
+        if ($formBooking->isSubmitted() && $form->isValid()) {
+            $kittenId = $request->request->get('kitten_id');
+            $kitten = $entityManager->getRepository(Kitten::class)->find($kittenId);
+
+            if (!$kitten) {
+                throw $this->createNotFoundException('Kitten not found');
+            }
+
+            $booking->setKitten($kitten);
+            $booking->setUser($this->getUser());
+            $booking->setCreatedAt(new \DateTime());
+            $booking->setStatus('В ожидании');
+
+            $entityManager->persist($booking);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Ваша заявка отправлена!');
+            return $this->redirectToRoute('app_home');
+        }
+
         return $this->render('home/index.html.twig', [
             'litter' => $litter,
             'kittens' => $kittens,
             'mother' => $mom,
             'father' => $dad,
             'form' => $form->createView(),
+            'formBooking' => $formBooking->createView(),
             'bookings' => $bookingRepository->findAll(),
         ]);
     }
