582efece5c31f4ef43d46e7c92d5f8acf93ddd57
Sofiya <sofik_shu2@mail.ru>
2025-04-30 01:07:04 +0300

    added mark to view

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,6 +3,8 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Booking;
+use App\Repository\BookingRepository;
+use App\Repository\GuestRequestRepository;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -24,6 +26,11 @@ class DashboardController extends AbstractDashboardController
         return $this->render('admin/dashboard.html.twig');
     }
 
+    public function __construct(
+        private BookingRepository $bookingRepository,
+        private GuestRequestRepository $guestRequestRepository
+    ) {}
+
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
@@ -32,16 +39,35 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
+        // Проверяем наличие новых уведомлений для каждой сущности
+        $hasNewBookings = $this->bookingRepository->hasUnseen();
+        $hasNewRequests = $this->guestRequestRepository->hasUnseen();
+
+        // Проверяем наличие уведомлений для всех сущностей
+        $hasNewEntities = $hasNewBookings || $hasNewRequests;
+
+        // Создаем родительское меню с уведомлением, если есть новые записи
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::subMenu('Entities', 'fa fa-home')->setSubItems([
-            MenuItem::linkToCrud('Bookings', 'fas fa-list', Booking::class),
+
+        yield MenuItem::subMenu('Entities' . ($hasNewEntities ? ' 🔴' : ''), 'fa fa-home')->setSubItems([
+            MenuItem::linkToCrud(
+                'Bookings' . ($hasNewBookings ? ' 🔴' : ''),
+                'fas fa-list',
+                Booking::class
+            ),
+            MenuItem::linkToCrud(
+                'Guest requests' . ($hasNewRequests ? ' 🔴' : ''),
+                'fas fa-list',
+                GuestRequest::class
+            ),
             MenuItem::linkToCrud('Cats', 'fas fa-list', Cat::class),
             MenuItem::linkToCrud('Colors', 'fas fa-list', Color::class),
-            MenuItem::linkToCrud('Guest requests', 'fas fa-list', GuestRequest::class),
             MenuItem::linkToCrud('Kittens', 'fas fa-list', Kitten::class),
             MenuItem::linkToCrud('Liters', 'fas fa-list', Litter::class),
             MenuItem::linkToCrud('Kittens statuses', 'fas fa-list', KittenStatus::class),
         ]);
+
         yield MenuItem::linkToRoute('Homepage', 'fa fa-home', 'admin_select_litter');
     }
+
 }
